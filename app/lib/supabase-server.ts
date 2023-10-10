23bb71a0e3f50afb8ac409721a14d73692a78fa1
h.yaghooti <hamedyaghooti1992@gmail.com>
2023-10-10 16:22:03 +0330

    create supabase variable from different method

---
+++ app/lib/supabase-server.ts
@@ -0,0 +1,6 @@
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+
+const supabase = createServerComponentClient({ cookies });
+
+export default supabase;
